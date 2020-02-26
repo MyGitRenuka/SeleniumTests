1de0b1d69c5b7d70e20c89aa07b95100e73a50d2
renuka <renuka.link@gmail.com>
2020-02-26 15:22:23 +0530

    Updated test init to execute on firefox and IE

--- src/test/java/com/selTestng/init/TestInit.java
+++ src/test/java/com/selTestng/init/TestInit.java
@@ -2,10 +2,13 @@ package com.selTestng.init;
 
 import java.io.File;
 import java.io.IOException;
+import java.net.MalformedURLException;
+import java.net.URL;
 import java.nio.file.Files;
 import java.text.SimpleDateFormat;
 
 import org.openqa.selenium.OutputType;
+import org.openqa.selenium.Platform;
 import org.openqa.selenium.TakesScreenshot;
 
 import java.util.Calendar;
@@ -13,6 +16,14 @@ import java.util.concurrent.TimeUnit;
 import org.apache.log4j.Logger;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.chrome.ChromeDriver;
+import org.openqa.selenium.chrome.ChromeOptions;
+import org.openqa.selenium.firefox.FirefoxBinary;
+import org.openqa.selenium.firefox.FirefoxDriver;
+import org.openqa.selenium.firefox.FirefoxOptions;
+import org.openqa.selenium.ie.InternetExplorerDriver;
+import org.openqa.selenium.ie.InternetExplorerOptions;
+import org.openqa.selenium.remote.DesiredCapabilities;
+import org.openqa.selenium.remote.RemoteWebDriver;
 import org.openqa.selenium.support.ui.WebDriverWait;
 import org.testng.ITestResult;
 import org.testng.Reporter;
@@ -20,6 +31,7 @@ import org.testng.annotations.AfterMethod;
 import org.testng.annotations.AfterTest;
 import org.testng.annotations.BeforeSuite;
 import org.testng.annotations.BeforeTest;
+import org.testng.annotations.Parameters;
 
 import com.aventstack.extentreports.ExtentReports;
 import com.aventstack.extentreports.ExtentTest;
@@ -53,35 +65,60 @@ public class TestInit {
 	}
 	
 	@BeforeTest
-	public void setWebdriver() {
+	@Parameters("browser")
+	public void setWebdriver(String browser) {
 		Config.setProperties();
-		browser = Config.testConfig.getProperty("browser").toLowerCase();
-		switch(browser)
+		if(browser.equalsIgnoreCase("default"))
+			TestInit.browser = Config.testConfig.getProperty("browser").toLowerCase();
+		else
+			TestInit.browser=browser;
+		
+		
+		switch(TestInit.browser.toLowerCase())
 		{
 			case "chrome":
 				System.setProperty("webdriver.chrome.driver", System.getProperty("user.dir") + 
 						Config.testConfig.getProperty("chromeDriver"));	    
 				System.out.println(System.getProperty("webdriver.chrome.driver"));
 				TestInit.driver = new ChromeDriver();
-				break;		    
-			/*FIREFOX:
-			IE:
-			 */
+				initialiseDriverWaits(driver,"chrome");
+				break;	
+			case "firefox":
+				System.setProperty("webdriver.gecko.driver", System.getProperty("user.dir") + 
+						Config.testConfig.getProperty("geckoDriver"));	    
+				System.out.println(System.getProperty("webdriver.gecko.driver"));
+				FirefoxOptions options1 = new FirefoxOptions();
+				options1.addPreference("marionette", true);
+				TestInit.driver = new FirefoxDriver(options1);
+				initialiseDriverWaits(driver,"firefox");
+				break;	
+			case "ie":
+				System.setProperty("webdriver.ie.driver", System.getProperty("user.dir") + 
+						Config.testConfig.getProperty("IEDriver"));	    
+				System.out.println(System.getProperty("webdriver.ie.driver"));
+				InternetExplorerOptions options = new InternetExplorerOptions();
+				options.introduceFlakinessByIgnoringSecurityDomains();
+				options.requireWindowFocus();
+				options.destructivelyEnsureCleanSession();
+				driver = new InternetExplorerDriver(options);
+				initialiseDriverWaits(driver,"IE");
+				break;	
 			default: 
 				System.setProperty("webdriver.chrome.driver", System.getProperty("user.dir") + 
 						Config.testConfig.getProperty("chromeDriver"));	    
 				System.out.println(System.getProperty("webdriver.chrome.driver"));
-				TestInit.driver = new ChromeDriver();							
+				TestInit.driver = new ChromeDriver();
+				initialiseDriverWaits(driver,"chrome");
 		}		
-		initialiseDriverWaits(driver);	
+			
 		test = extent.createTest(getClass().getSimpleName());
 	}
 	
-	
+
 	@AfterMethod
 	public void afterMethod(ITestResult result) throws IOException{
 		
-		log.info("TEST || " + result.getName() + " || STATUS " + result.getStatus());
+		log.info("TEST || " + result.getName() + " || STATUS || " + result.getStatus());
 		if(result.getStatus() == ITestResult.FAILURE){
 			test.log(Status.FAIL, result.getThrowable());
 			String imagePath = captureScreen(driver,result.getName());
@@ -100,7 +137,7 @@ public class TestInit {
 	@AfterTest
 	public void afterTest() throws Exception{
 		if(driver!=null){
-			driver.quit();
+			driver.close();
 		}
 		log.info("############# Test execution completed and all browser windows closed ############");
 	}
@@ -125,13 +162,13 @@ public class TestInit {
 		log.info("Wait time unit successfully set to " + unitFromConfig);
 	}
 	
-	public void initialiseDriverWaits(WebDriver driver) {
+	public void initialiseDriverWaits(WebDriver driver,String browser) {
 		setWaitTimeUnit();
-		setImplicitWait(driver);
-		setPageLoadTime(driver);
+		setImplicitWait(driver,browser);
+		setPageLoadTime(driver,browser);
 		explicitWait=readWaitTimeFromConfig("explicitWait");
 		deleteAllCookies(driver);
-		log.info("Successfully initialised the webdriver");
+		log.info("Successfully initialised the webdriver for browser " + browser);
 	}
 	
 	public long readWaitTimeFromConfig(String key) {
@@ -152,14 +189,14 @@ public class TestInit {
 		return  new WebDriverWait(driver, timeout);	
 	}
 	
-	public void setImplicitWait(WebDriver driver) {
+	public void setImplicitWait(WebDriver driver,String browser) {
 		long timeout = readWaitTimeFromConfig("implicitWait");
 		driver.manage().timeouts().implicitlyWait(timeout,waitTimeUnit);
 		log.info(browser + " driver initialized with implicit wait time [" + timeout + "] " +
 				waitTimeUnit.name().toLowerCase());
 	}
 	
-	public void setPageLoadTime(WebDriver driver) {
+	public void setPageLoadTime(WebDriver driver,String browser) {
 		long timeout = readWaitTimeFromConfig("pageLoadTime");
 		driver.manage().timeouts().pageLoadTimeout(timeout,waitTimeUnit);
 		log.info(browser + " driver initialized with page load time [" + timeout +"] " + 
